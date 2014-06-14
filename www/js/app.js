6b652fa2ad6476d79ea0c2c08711bc0597d4b121
Jordan Lewis Skole <jordan@robotcouture.com>
2014-06-14 14:41:46 -0400

    beers default view

--- www/js/app.js
+++ www/js/app.js
@@ -112,7 +112,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
     })
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/beers');
 
 })
 
